59167b5ac2c3ae1a7d7c441e6ff8f1803643ba9d
adoronin158@gmail.com <adoronin158@gmail.com>
2022-10-05 20:47:23 +0300

    123

---
+++ ViewModel/MovieViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Poster.Model.DBModels;
+using Poster.Model.Tools;
+using Poster.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace Poster.ViewModel
+{
+    class MovieViewModel : INotifyPropertyChanged
+    {
+        private CommandTemplate _openTicketWindow;
+        private Window _window;
+        private Movie _movie;
+        public MovieViewModel (Window window,Movie movie)
+        {
+            _window = window;
+            _movie = movie;
+        }
+
+        public CommandTemplate CreateTicketWindow
+        {
+            get
+            {
+                if (_openTicketWindow == null)
+                    _openTicketWindow = new CommandTemplate(AddTicketWindow);
+                return _openTicketWindow;
+            }
+        }
+
+        public void AddTicketWindow(object obj)
+        {
+            TicketWindow ticketWindow = new TicketWindow();
+            TicketViewModel ticketWindowViewModel = new TicketViewModel();
+
+            ticketWindow.DataContext = ticketWindowViewModel;
+            ticketWindow.ShowDialog();
+            _window.Show();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ ViewModel/PosterViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Poster.Model;
+using Poster.Model.DBModels;
+using Poster.Model.Tools;
+using Poster.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace Poster.ViewModel
+{
+    class PosterViewModel : INotifyPropertyChanged
+    {
+        private Movie _selectedMovie;
+        private PosterData _model;
+       
+        public ObservableCollection<PosterMovieViewModel> PosterMovies { get; set; }
+
+        public ObservableCollection<IReadOnlyMovie> Movies { get; private set; }
+
+        public Movie SelectedMovie
+        {
+            get => _selectedMovie;
+            set
+            {
+                _selectedMovie = value;
+                OnPropertyChanged(nameof(SelectedMovie));
+            }
+        }
+
+        public PosterViewModel(PosterData model)
+        {
+            _model = model;
+            Movies = new ObservableCollection<IReadOnlyMovie>(_model.GetAllMovies());
+        }
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    internal class PosterMovieViewModel
+    {
+        Movie _movie;
+        private CommandTemplate _openMovieWindow;
+        private Window _window;
+
+        public PosterMovieViewModel(Window window,Movie movie)
+        {
+            _window = window;
+            _movie = movie;
+        }
+        public CommandTemplate OpenMovieWindow
+        {
+            get
+            {
+                if (_openMovieWindow == null)
+                    _openMovieWindow = new CommandTemplate(AddMovieWindow);
+                return _openMovieWindow;
+            }
+        }
+
+        public void AddMovieWindow(object obj)
+        {
+            MovieWindow movieWindow = new MovieWindow();
+            MovieViewModel movieViewModel = new MovieViewModel(_window,_movie);
+
+            _window.Hide();
+
+            movieWindow.DataContext = movieViewModel;
+            movieWindow.ShowDialog();
+            _window.Show();
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+
+
+}
